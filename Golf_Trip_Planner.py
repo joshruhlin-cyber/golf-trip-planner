be124e924657d6afee92e5198fa4acb723f10ab9
Josh Ruhlin <jruhlin@insight2profit.com>
2026-05-28 11:26:16 -0500

    Capitalize city entered

--- Golf_Trip_Planner.py
+++ Golf_Trip_Planner.py
@@ -190,6 +190,8 @@ st.markdown("""
 st.markdown('<div class="section-header">📋 Trip Details</div>', unsafe_allow_html=True)
 
 city = st.text_input("📍 Where are you planning to golf?", placeholder="e.g. Scottsdale, AZ")
+if city:
+    city = city.title()
 dates = st.text_input("📅 What are the dates of your trip?", placeholder="e.g. Oct 11-13")
 if dates:
     try:
